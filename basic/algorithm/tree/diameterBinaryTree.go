34bd86f5c39c962f450259e40a9bf3bfb5475dcb
BowenChen <bowenchen886@gmail.com>
2017-03-22 09:51:17 +0800

    add diameterBinaryTree code

--- basic/algorithm/tree/diameterBinaryTree.go
+++ basic/algorithm/tree/diameterBinaryTree.go
@@ -1,7 +1,45 @@
-// Given a binary tree 
+// Given a binary tree
 //           1
 //          / \
 //         2   3
 //        / \
 //       4   5
 // Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+
+
+//Definition for a binary tree node.
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func diameterBinaryTree(root *TreeNode) int {
+    i := 0
+    if root == nil {
+        return i
+    }
+
+    if root.Left == nil && root.Right == nil {
+        return i
+    }
+
+    i += diameterOneBinaryTree(root.Left)
+    i += diameterOneBinaryTree(root.Right)
+    return i
+}
+
+func diameterOneBinaryTree(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+
+    left := diameterOneBinaryTree(root.Left)
+    right := diameterOneBinaryTree(root.Right)
+
+    if left > right {
+      return left + 1
+    } else {
+      return right + 1
+    }
+}
